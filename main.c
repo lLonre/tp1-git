8bb1b4e3118cc7805ae603a58fc05b42f4ec75c2
bernoldwong <bernold.wong@esme.fr>
2024-10-03 14:57:55 +0200

    Modification de main.c

--- main.c
+++ main.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    printf("Bonjour, voici la version améliorée de mon programme C !\n");
-    return 0;
-    printf("Nouvelle ligne ajoutée.\n");
-dsfsdfsfsdfds
-}
+Bonjour
